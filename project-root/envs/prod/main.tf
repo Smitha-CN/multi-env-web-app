85143926f0d7d9796bbc8746f4dbf74b63ba572f
Smitha-CN <smithamalthi@gmail.com>
2025-06-12 19:35:27 -0400

    Update main.tf

--- project-root/envs/prod/main.tf
+++ project-root/envs/prod/main.tf
@@ -1,17 +1,6 @@
-provider "aws" {
-  region = "us-west-1"
+module "s3" {
+  source      = "../../modules/s3"
+  bucket_name = var.bucket_name
 }
 
-module "vpc" {
-  source      = "../../modules/vpc"
-  cidr_block  = var.vpc_cidr
-  environment = var.environment
-}
-
-module "ec2" {
-  source        = "../../modules/ec2"
-  instance_type = var.instance_type
-  environment   = var.environment
-  vpc_id        = module.vpc.vpc_id
-  subnet_id     = module.vpc.public_subnet_id
-}
+variable "bucket_name" {}
