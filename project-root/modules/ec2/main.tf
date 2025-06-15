7d42f129b2e44da9ce033ab3e754fcf8ee6beec2
Smitha-CN <smithamalthi@gmail.com>
2025-06-15 19:29:30 -0400

    Update main.tf

--- project-root/modules/ec2/main.tf
+++ project-root/modules/ec2/main.tf
@@ -1,61 +1,11 @@
 resource "aws_instance" "this" {
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.this.id]
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  key_name               = var.key_name
 
   tags = {
-    Name        = "${var.environment}-ec2"
-    Environment = var.environment
+    Name = "${var.env_name}-ec2-instance"
   }
-}
-
-resource "aws_security_group" "this" {
-  name        = "${var.environment}-ec2-sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.environment}-sg"
-  }
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["137112412989"] # Amazon
 }
