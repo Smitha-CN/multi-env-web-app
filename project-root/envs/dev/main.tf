98d06006d51be7bef2c7d82d99224b7d71dc0dae
Smitha-CN <smithamalthi@gmail.com>
2025-06-15 19:30:11 -0400

    Update main.tf

--- project-root/envs/dev/main.tf
+++ project-root/envs/dev/main.tf
@@ -1,6 +1,13 @@
-module "s3" {
-  source      = "../../modules/s3"
-  bucket_name = var.bucket_name
+provider "aws" {
+  region = "ap-south-1"
 }
 
-variable "bucket_name" {}
+module "ec2" {
+  source             = "../../modules/ec2"
+  ami_id             = var.ami_id
+  instance_type      = var.instance_type
+  subnet_id          = var.subnet_id
+  security_group_ids = var.security_group_ids
+  key_name           = var.key_name
+  env_name           = "dev"
+}
