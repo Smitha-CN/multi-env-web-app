8f4def0ad2f0d87c66ec826755b7755fc82c176d
Smitha-CN <smithamalthi@gmail.com>
2025-06-15 21:05:12 -0400

    Delete project-root/envs/prod/backend.tf

--- project-root/envs/prod/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-states"       # your actual bucket name
-    key            = "prod/ec2.tfstate"           # or prod/ec2.tfstate
-    region         = "eu-west-1"                 # ✅ must match the actual bucket region
-    dynamodb_table = "terraform-locks"           # if you're using state locking
-    encrypt        = true
-  }
-}
