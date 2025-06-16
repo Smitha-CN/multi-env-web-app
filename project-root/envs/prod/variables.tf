1fa36e5f5daf6fc7fc7926df21c08e5fd9353902
Smitha-CN <smithamalthi@gmail.com>
2025-06-15 20:16:36 -0400

    Update variables.tf

--- project-root/envs/prod/variables.tf
+++ project-root/envs/prod/variables.tf
@@ -1,14 +1,19 @@
-variable "environment" {
-  description = "Environment name (e.g., dev, prod)"
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
   type        = string
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instance"
   type        = string
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
+variable "security_group_ids" {
+  description = "List of security group IDs for the EC2 instance"
+  type        = list(string)
+}
+
+variable "key_name" {
+  description = "Key pair name for EC2 SSH access"
   type        = string
 }
